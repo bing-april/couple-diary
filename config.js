269a8a0fd77b7a74a75906907342fd61436df648
bing-april <bingting.lai@gmail.com>
2026-05-02 18:49:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
   SUPABASE_URL: "https://ydjtukznffxoqnuhmdga.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_x9yVCmepQkmAZEEMAtlKQQ_qN5q6eJd",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlkanR1a3puZmZ4b3FudWhtZGdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3MDU4NzIsImV4cCI6MjA5MzI4MTg3Mn0.7EXgOECk56nn_KtSmF-Js7qUsHcIJimaJ_Ohn3Aiqs0",
 };
